cbb89e65f53625bec03be7f7526d37d26c2de4d0
tetracalibers <tomix05may@gmail.com>
2024-11-20 18:38:38 +0900

    rendererの実装

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -2,3 +2,28 @@
 // render のロジックのみを持つオブジェクトを生成するためのファクトリ関数を実装する
 // Node(DOMに限らず)を扱うオブジェクトは factory の関数の引数として受け取るようにする
 //
+
+export interface RendererOptions<HostNode = RendererNode> {
+  setElementText(node: HostNode, text: string): void
+}
+
+export interface RendererNode {
+  [key: string]: any
+}
+
+export interface RendererElement extends RendererNode {}
+
+export type RootRenderFunction<HostElement = RendererElement> = (
+  message: string,
+  container: HostElement
+) => void
+
+export function createRenderer(options: RendererOptions) {
+  const { setElementText: hostSetElementText } = options
+
+  const render: RootRenderFunction = (message, container) => {
+    hostSetElementText(container, message) // 今回はメッセージを挿入するだけなのでこういう実装になっている
+  }
+
+  return { render }
+}
--- packages/runtime-dom/index.ts
+++ packages/runtime-dom/index.ts
@@ -1,3 +1,8 @@
 //
 // nodeOps と renderer のファクトリをもとに renderer を完成させる
 //
+
+import { createRenderer } from '../runtime-core'
+import { nodeOps } from './nodeOps'
+
+const { render } = createRenderer(nodeOps)
