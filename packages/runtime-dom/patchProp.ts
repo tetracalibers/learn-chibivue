c9ae8025123fc910f0fdb6d1c22d88ca5d4a1696
tetracalibers <tomix05may@gmail.com>
2024-11-22 07:52:36 +0900

    補足コメント追加

--- packages/runtime-dom/patchProp.ts
+++ packages/runtime-dom/patchProp.ts
@@ -4,12 +4,13 @@ import { patchEvent } from './modules/events'
 
 type DOMRendererOptions = RendererOptions<Node, Element>
 
+// onから始まる属性名かどうかを判定する
 const onRE = /^on[^a-z]/
 export const isOn = (key: string) => onRE.test(key)
 
 export const patchProp: DOMRendererOptions['patchProp'] = (el, key, value) => {
   if (isOn(key)) {
-    patchEvent(el, key, value)
+    patchEvent(el, key, value) // onから始まる属性はイベントハンドラとして処理
   } else {
     patchAttr(el, key, value)
   }
