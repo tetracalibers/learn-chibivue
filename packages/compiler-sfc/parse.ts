bc5e961c9e3e085c6fe52cc2947c06f6a7e8aa40
tetracalibers <tomix05may@gmail.com>
2024-11-25 07:53:16 +0900

    SFCを表現するオブジェクトを定義

---
+++ packages/compiler-sfc/parse.ts
@@ -0,0 +1,28 @@
+import { SourceLocation } from '../compiler-core'
+
+export interface SFCDescriptor {
+  id: string
+  filename: string
+  source: string
+  template: SFCTemplateBlock | null
+  script: SFCScriptBlock | null
+  styles: SFCStyleBlock[]
+}
+
+export interface SFCBlock {
+  type: string
+  content: string
+  loc: SourceLocation
+}
+
+export interface SFCTemplateBlock extends SFCBlock {
+  type: 'template'
+}
+
+export interface SFCScriptBlock extends SFCBlock {
+  type: 'script'
+}
+
+export declare interface SFCStyleBlock extends SFCBlock {
+  type: 'style'
+}
