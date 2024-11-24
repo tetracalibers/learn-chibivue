587135dbc5b710b304f74081e76b724a8ca991b9
tetracalibers <tomix05may@gmail.com>
2024-11-24 23:15:07 +0900

    parse関数のガワを作成

---
+++ packages/compiler-core/parse.ts
@@ -0,0 +1,9 @@
+import { TemplateChildNode } from './ast'
+
+// template の文字列を AST に変換する
+export const baseParse = (
+  content: string
+): { children: TemplateChildNode[] } => {
+  // TODO:
+  return { children: [] }
+}
