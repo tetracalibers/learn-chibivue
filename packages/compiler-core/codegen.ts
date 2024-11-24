a7902905dd0e304fb3e8247e3e9d33cc2c82e346
tetracalibers <tomix05may@gmail.com>
2024-11-25 08:22:12 +0900

    template部分のコンパイル：Viteではwith文を使えないので、Viteかブラウザかで生成するコードを分岐

--- packages/compiler-core/codegen.ts
+++ packages/compiler-core/codegen.ts
@@ -19,39 +19,57 @@ export const generate = (
   },
   option: Required<CompilerOptions>
 ): string => {
+  // ブラウザモードでない場合には with 文を含まないコードを生成
+  // Vite は ESM を扱う都合上，非厳格モード (sloppy モード) でのみ動作するコードを処理できず，with 文を扱うことができないため
   return `${option.isBrowser ? 'return ' : ''}function render(_ctx) {
-  const { h } = ChibiVue;
-  return ${genNode(children[0])};
+    ${option.isBrowser ? 'with (_ctx) {' : ''}
+      const { h } = ChibiVue;
+      return ${genNode(children[0], option)};
+    ${option.isBrowser ? '}' : ''}
 }`
 }
 
-const genNode = (node: TemplateChildNode): string => {
+const genNode = (
+  node: TemplateChildNode,
+  option: Required<CompilerOptions>
+): string => {
   switch (node.type) {
     case NodeTypes.ELEMENT:
-      return genElement(node)
+      return genElement(node, option)
     case NodeTypes.TEXT:
       return genText(node)
     case NodeTypes.INTERPOLATION:
-      return genInterpolation(node)
+      return genInterpolation(node, option)
     default:
       return ''
   }
 }
 
-const genElement = (el: ElementNode): string => {
+const genElement = (
+  el: ElementNode,
+  option: Required<CompilerOptions>
+): string => {
   return `h("${el.tag}", {${el.props
-    .map((prop) => genProp(prop))
-    .join(', ')}}, [${el.children.map((it) => genNode(it)).join(', ')}])`
+    .map((prop) => genProp(prop, option))
+    .join(
+      ', '
+    )}}, [${el.children.map((it) => genNode(it, option)).join(', ')}])`
 }
 
-const genProp = (prop: AttributeNode | DirectiveNode): string => {
+const genProp = (
+  prop: AttributeNode | DirectiveNode,
+  option: Required<CompilerOptions>
+): string => {
   switch (prop.type) {
     case NodeTypes.ATTRIBUTE:
       return `${prop.name}: "${prop.value?.content}"`
     case NodeTypes.DIRECTIVE: {
       switch (prop.name) {
         case 'on':
-          return `${toHandlerKey(prop.arg)}: ${prop.exp}`
+          // バインド対象のデータに関しては、 with 文を使用せずに prefix として _cxt.　を付与する形で制御
+          return `${toHandlerKey(prop.arg)}: ${
+            option.isBrowser ? '' : '_ctx.'
+          }${prop.exp}`
         default:
           // TODO: other directives
           throw new Error(`unexpected directive name. got "${prop.name}"`)
@@ -66,6 +84,10 @@ const genText = (text: TextNode): string => {
   return `\`${text.content}\``
 }
 
-const genInterpolation = (node: InterpolationNode): string => {
-  return `${node.content}`
+const genInterpolation = (
+  node: InterpolationNode,
+  option: Required<CompilerOptions>
+): string => {
+  // バインド対象のデータに関しては、 with 文を使用せずに prefix として _cxt.　を付与する形で制御
+  return `${option.isBrowser ? '' : '_ctx.'}${node.content}`
 }
