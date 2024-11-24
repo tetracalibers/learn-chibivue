f01e0932ee22e6e70f1e6ebb9f1a828ab7684162
tetracalibers <tomix05may@gmail.com>
2024-11-25 07:12:38 +0900

    [playground] setup関数で返したstateをマスタッシュ構文で埋め込んで表示できるようになった

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,9 +1,14 @@
-import { createApp } from 'chibivue'
+import { createApp, reactive } from 'chibivue'
 
 const app = createApp({
+  setup() {
+    const state = reactive({ message: 'Hello, chibivue!' })
+
+    return { state }
+  },
   template: `
     <div class="container" style="text-align: center">
-      <h2>Hello, chibivue!</h2>
+      <h2>{{ state.message }}</h2>
       <img
         width="150px"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png"
