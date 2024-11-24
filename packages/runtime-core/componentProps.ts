67c48d2f93b350802a8a36fb6ebcedaccf7731b0
tetracalibers <tomix05may@gmail.com>
2024-11-24 20:43:01 +0900

    コンポーネントのマウント時にpropsを初期化

--- packages/runtime-core/componentProps.ts
+++ packages/runtime-core/componentProps.ts
@@ -1,3 +1,6 @@
+import { reactive } from '../reactivity'
+import { ComponentInternalInstance, Data } from './component'
+
 export type Props = Record<string, PropOptions | null>
 
 export interface PropOptions<T = any> {
@@ -7,3 +10,31 @@ export interface PropOptions<T = any> {
 }
 
 export type PropType<T> = { new (...args: any[]): T & {} }
+
+export function initProps(
+  instance: ComponentInternalInstance,
+  rawProps: Data | null
+) {
+  const props: Data = {}
+  // vnode が保持している props を propsOptions を元にフィルターする
+  setFullProps(instance, rawProps, props)
+  // フィルターしてできたオブジェクトを reactive 関数によってリアクティブなオブジェクトにする
+  instance.props = reactive(props)
+}
+
+function setFullProps(
+  instance: ComponentInternalInstance,
+  rawProps: Data | null,
+  props: Data
+) {
+  const options = instance.propsOptions
+
+  if (rawProps) {
+    for (let key in rawProps) {
+      const value = rawProps[key]
+      if (options && options.hasOwnProperty(key)) {
+        props[key] = value
+      }
+    }
+  }
+}
