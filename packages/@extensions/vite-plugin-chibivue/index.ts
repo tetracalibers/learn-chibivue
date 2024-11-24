cdeef7700e852464500ef1a83bc5b36579d39bc7
tetracalibers <tomix05may@gmail.com>
2024-11-25 07:50:55 +0900

    transformの対象を「*.vue」に限定

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,11 +1,18 @@
 import type { Plugin } from 'vite'
+import { createFilter } from 'vite'
 
 export default function vitePluginChibivue(): Plugin {
+  const filter = createFilter(/\.vue$/)
+
   return {
     name: 'vite:chibivue',
 
     transform(code, id) {
-      return { code }
+      // transformの対象を「*.vue」に限定
+      if (!filter(id)) return
+
+      // vueファイルだった場合はファイル内容にtransform
+      return { code: `export default {}` }
     },
   }
 }
