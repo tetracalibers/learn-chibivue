347fa1e48fdbe054c5710bebb46c533d0c9b4be0
tetracalibers <tomix05may@gmail.com>
2024-11-24 20:03:33 +0900

    コンポーネントのインスタンスの型を定義

--- packages/runtime-core/component.ts
+++ packages/runtime-core/component.ts
@@ -1,3 +1,20 @@
+import { ReactiveEffect } from '../reactivity'
 import { ComponentOptions } from './componentOptions'
+import { VNode, VNodeChild } from './vnode'
 
 export type Component = ComponentOptions
+
+export interface ComponentInternalInstance {
+  type: Component // 元となるユーザー定義のコンポーネント
+  vnode: VNode
+  subTree: VNode // レンダリング結果であるVNode 1（差分を比較するためのもの）
+  next: VNode | null // レンダリング結果である VNode 2（差分を比較するためのもの）
+  effect: ReactiveEffect
+  render: InternalRenderFunction
+  update: () => void
+  isMounted: boolean
+}
+
+export type InternalRenderFunction = {
+  (): VNodeChild
+}
