b18909912de522ed27c4fa5a43f0d04c3b94b06c
tetracalibers <tomix05may@gmail.com>
2024-11-25 08:02:21 +0900

    実際にSFCをplugin側でパース

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,5 +1,6 @@
 import type { Plugin } from 'vite'
 import { createFilter } from 'vite'
+import { parse } from '../../compiler-sfc'
 
 export default function vitePluginChibivue(): Plugin {
   const filter = createFilter(/\.vue$/)
@@ -10,6 +11,12 @@ export default function vitePluginChibivue(): Plugin {
     transform(code, id) {
       // transformの対象を「*.vue」に限定
       if (!filter(id)) return
+
+      const { descriptor } = parse(code, { filename: id })
+      console.log(
+        '🚀 ~ file: index.ts:14 ~ transform ~ descriptor:',
+        descriptor
+      )
 
       // vueファイルだった場合はファイル内容にtransform
       return { code: `export default {}` }
