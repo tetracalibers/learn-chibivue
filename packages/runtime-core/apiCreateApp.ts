1c76a99c5c1e3a6e15ff803ed781f188cf0fd829
tetracalibers <tomix05may@gmail.com>
2024-11-24 19:30:00 +0900

    createAppAPIのmountに書いていた処理を一部render関数に移植（２つのvnodeを保持できるように）

--- packages/runtime-core/apiCreateApp.ts
+++ packages/runtime-core/apiCreateApp.ts
@@ -16,25 +16,7 @@ export function createAppAPI<HostElement>(
   return function createApp(rootComponent) {
     const app: App = {
       mount(rootContainer: HostElement) {
-        // setup関数が実行された時点で reactive proxy が生成される
-        // componentRender は setup 関数の戻り値である render 関数
-        // - render 関数は proxy によって作られたオブジェクトを参照している
-        // - 実際に rerder 関数が走った時、target の getter 関数が実行され，track が実行されるようになっている
-        const componentRender = rootComponent.setup!()
-
-        const updateComponent = () => {
-          const vnode = componentRender()
-          render(vnode, rootContainer)
-        }
-
-        // updateComponent を渡して ReactiveEffect (Observer 側)を生成する
-        const effect = new ReactiveEffect(updateComponent)
-        // effect を実行
-        // 1. activeEffect に updateComponent (を持った ReactiveEffect) が設定される
-        // 2. この状態で track が走ると、targetMap に target と updateComponent (を持った ReactiveEffect) のマップが登録される（リアクティブの形成）
-        // 3. この状態で target が書き換えられ（setterが実行され）、trigger が走ると、targetMap から effect(今回の例だと updateComponent)をみつけ、実行する
-        // これで画面の更新が行われる
-        effect.run()
+        render(rootComponent, rootContainer)
       },
     }
 
--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -3,6 +3,8 @@
 // Node(DOMに限らず)を扱うオブジェクトは factory の関数の引数として受け取るようにする
 //
 
+import { ReactiveEffect } from '../reactivity'
+import { Component } from './component'
 import { VNode, Text, normalizeVNode } from './vnode'
 
 export interface RendererOptions<
@@ -23,7 +25,7 @@ export interface RendererNode {
 export interface RendererElement extends RendererNode {}
 
 export type RootRenderFunction<HostElement = RendererElement> = (
-  message: string,
+  vnode: Component,
   container: HostElement
 ) => void
 
@@ -91,8 +93,29 @@ export function createRenderer(options: RendererOptions) {
     }
   }
 
-  const render: RootRenderFunction = (vnode, container) => {
-    // TODO
+  const render: RootRenderFunction = (rootComponent, container) => {
+    // setup関数が実行された時点で reactive proxy が生成される
+    // componentRender は setup 関数の戻り値である render 関数
+    // - render 関数は proxy によって作られたオブジェクトを参照している
+    // - 実際に rerder 関数が走った時、target の getter 関数が実行され，track が実行されるようになっている
+    const componentRender = rootComponent.setup!()
+
+    let n1: VNode | null = null
+
+    const updateComponent = () => {
+      const n2 = componentRender()
+      patch(n1, n2, container)
+      n1 = n2
+    }
+
+    // updateComponent を渡して ReactiveEffect (Observer 側)を生成する
+    const effect = new ReactiveEffect(updateComponent)
+    // effect を実行
+    // 1. activeEffect に updateComponent (を持った ReactiveEffect) が設定される
+    // 2. この状態で track が走ると、targetMap に target と updateComponent (を持った ReactiveEffect) のマップが登録される（リアクティブの形成）
+    // 3. この状態で target が書き換えられ（setterが実行され）、trigger が走ると、targetMap から effect(今回の例だと updateComponent)をみつけ、実行する
+    // これで画面の更新が行われる
+    effect.run()
   }
 
   return { render }
