0f1aed00007789f15df244afc7eab2e6b475e050
tetracalibers <tomix05may@gmail.com>
2024-11-24 23:25:51 +0900

    parse: TextNodeの解析

--- packages/compiler-core/parse.ts
+++ packages/compiler-core/parse.ts
@@ -1,4 +1,11 @@
-import { ElementNode, NodeTypes, TemplateChildNode } from './ast'
+import {
+  ElementNode,
+  NodeTypes,
+  Position,
+  SourceLocation,
+  TemplateChildNode,
+  TextNode,
+} from './ast'
 
 // parse する際に使う状態
 // パース中に必要な情報をここにまとめる
@@ -58,7 +65,7 @@ function parseChildren(
 
     if (!node) {
       // 上記の条件に当てはまらなかった場合はTextNodeとしてパースする
-      // node = parseText(context)
+      node = parseText(context)
     }
 
     pushNode(nodes, node)
@@ -111,4 +118,89 @@ function startsWithEndTagOpen(source: string, tag: string): boolean {
     source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase() &&
     /[\t\r\n\f />]/.test(source[2 + tag.length] || '>')
   )
+}
+
+function parseText(context: ParserContext): TextNode {
+  // "<" (タグの開始(開始タグ終了タグ問わず))まで読み進め、何文字読んだかを元にTextデータの終了時点のindexを算出する
+  const endToken = '<'
+  let endIndex = context.source.length
+  const index = context.source.indexOf(endToken, 1)
+  if (index !== -1 && endIndex > index) {
+    endIndex = index
+  }
+
+  const start = getCursor(context) // これは loc 用
+
+  // endIndexの情報を元に Text データをパースする
+  const content = parseTextData(context, endIndex)
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
+    loc: getSelection(context, start),
+  }
+}
+
+// content と length を元に text を抽出する
+function parseTextData(context: ParserContext, length: number): string {
+  const rawText = context.source.slice(0, length)
+  advanceBy(context, length)
+  return rawText
+}
+
+function advanceBy(context: ParserContext, numberOfCharacters: number): void {
+  const { source } = context
+  advancePositionWithMutation(context, source, numberOfCharacters)
+  context.source = source.slice(numberOfCharacters)
+}
+
+function advanceSpaces(context: ParserContext): void {
+  const match = /^[\t\r\n\f ]+/.exec(context.source)
+  if (match) {
+    advanceBy(context, match[0].length)
+  }
+}
+
+// pos の計算を行う
+// 引数でもらった pos のオブジェクトを破壊的に更新する
+function advancePositionWithMutation(
+  pos: Position,
+  source: string,
+  numberOfCharacters: number = source.length
+): Position {
+  let linesCount = 0
+  let lastNewLinePos = -1
+  for (let i = 0; i < numberOfCharacters; i++) {
+    if (source.charCodeAt(i) === 10 /* newline char code */) {
+      linesCount++
+      lastNewLinePos = i
+    }
+  }
+
+  pos.offset += numberOfCharacters
+  pos.line += linesCount
+  pos.column =
+    lastNewLinePos === -1
+      ? pos.column + numberOfCharacters
+      : numberOfCharacters - lastNewLinePos
+
+  return pos
+}
+
+function getCursor(context: ParserContext): Position {
+  const { column, line, offset } = context
+  return { column, line, offset }
+}
+
+function getSelection(
+  context: ParserContext,
+  start: Position,
+  end?: Position
+): SourceLocation {
+  end = end || getCursor(context)
+  return {
+    start,
+    end,
+    source: context.originalSource.slice(start.offset, end.offset),
+  }
 }
