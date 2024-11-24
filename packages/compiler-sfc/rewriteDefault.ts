4f50312e0659ef7823a39c0d88f5cd95ea7f1ac2
tetracalibers <tomix05may@gmail.com>
2024-11-25 08:53:11 +0900

    script部分のコンパイル：exportの宣言が存在しない場合のハンドリング

--- packages/compiler-sfc/rewriteDefault.ts
+++ packages/compiler-sfc/rewriteDefault.ts
@@ -1,6 +1,19 @@
+const defaultExportRE = /((?:^|\n|;)\s*)export(\s*)default/
+const namedDefaultExportRE = /((?:^|\n|;)\s*)export(.+)(?:as)?(\s*)default/s
+
 // input: 対象のソースコード
 // as: 最終的にバインドしたい変数名
 export function rewriteDefault(input: string, as: string): string {
+  // export の宣言が存在しない場合のハンドリング
+  if (!hasDefaultExport(input)) {
+    // 空のオブジェクトをバインドして終了
+    return input + `\nconst ${as} = {}`
+  }
+
   // TODO:
   return ''
+}
+
+export function hasDefaultExport(input: string): boolean {
+  return defaultExportRE.test(input) || namedDefaultExportRE.test(input)
 }
