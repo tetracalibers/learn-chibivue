04240e3120e4db6ee18529aed072d49bd9526dad
tetracalibers <tomix05may@gmail.com>
2024-11-25 15:56:05 +0900

    [playground] 要素の順番の入れ替えの挙動を確認

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -2,9 +2,11 @@ import { createApp, h, reactive } from 'chibivue'
 
 const app = createApp({
   setup() {
-    const state = reactive({ list: ['A', 'B', 'C'] })
+    const state = reactive({
+      list: [{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }],
+    })
     const updateList = () => {
-      state.list = ['1', '2', '3', '4']
+      state.list = [{ key: 'a' }, { key: 'b' }, { key: 'd' }, { key: 'c' }]
     }
 
     return () =>
@@ -12,7 +14,7 @@ const app = createApp({
         h(
           'ul',
           {},
-          state.list.map((item) => h('li', {}, [item]))
+          state.list.map((item) => h('li', { key: item.key }, [item.key]))
         ),
         h('button', { onClick: updateList }, ['update']),
       ])
