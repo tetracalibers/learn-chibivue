4b296c0ff454f3be149abf3e51178a796e40bf75
tetracalibers <tomix05may@gmail.com>
2024-11-25 09:08:49 +0900

    script部分のコンパイル：export 対象を抜き出し，_sfc_main という変数に代入

--- packages/@extensions/vite-plugin-chibivue/index.ts
+++ packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,6 +1,6 @@
 import type { Plugin } from 'vite'
 import { createFilter } from 'vite'
-import { parse } from '../../compiler-sfc'
+import { parse, rewriteDefault } from '../../compiler-sfc'
 import { compile } from '../../compiler-dom'
 
 export default function vitePluginChibivue(): Plugin {
@@ -21,13 +21,21 @@ export default function vitePluginChibivue(): Plugin {
       outputs.push("import * as ChibiVue from 'chibivue'\n")
 
       const { descriptor } = parse(code, { filename: id })
+
+      const SFC_MAIN = '_sfc_main'
+      const scriptCode = rewriteDefault(
+        descriptor.script?.content ?? '',
+        SFC_MAIN
+      )
+      outputs.push(scriptCode)
+
       const templateCode = compile(descriptor.template?.content ?? '', {
         isBrowser: false,
       })
       outputs.push(templateCode)
 
       outputs.push('\n')
-      outputs.push(`export default { render }`)
+      outputs.push(`export default { ...${SFC_MAIN}, render }`)
 
       return { code: outputs.join('\n') }
     },
