b021ce6f99c4e1044013d5f3945910bfd452980f
tetracalibers <tomix05may@gmail.com>
2024-11-25 09:30:22 +0900

    デバッグ用のconsole.logを削除

--- packages/runtime-core/renderer.ts
+++ packages/runtime-core/renderer.ts
@@ -168,8 +168,6 @@ export function createRenderer(options: RendererOptions) {
     const componentUpdateFn = () => {
       const { render, setupState } = instance
 
-      console.log('render', render)
-
       if (!instance.isMounted) {
         // mount process
         const subTree = (instance.subTree = normalizeVNode(render(setupState)))
