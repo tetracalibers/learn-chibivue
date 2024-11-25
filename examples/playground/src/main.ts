452458875b982fe036eefba1e6392f60fdd3a885
tetracalibers <tomix05may@gmail.com>
2024-11-25 13:05:21 +0900

    [playground] 現時点でのパッチレンダリング実装のバグを確認する

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,7 +1,22 @@
-import { createApp } from 'chibivue'
-// @ts-ignore
-import App from './App.vue'
+import { createApp, h, reactive } from 'chibivue'
 
-const app = createApp(App)
+const app = createApp({
+  setup() {
+    const state = reactive({ list: ['A', 'B', 'C', 'D'] })
+    const updateList = () => {
+      state.list = ['1', '2', '3']
+    }
+
+    return () =>
+      h('div', { id: 'app' }, [
+        h(
+          'ul',
+          {},
+          state.list.map((item) => h('li', {}, [item]))
+        ),
+        h('button', { onClick: updateList }, ['update']),
+      ])
+  },
+})
 
 app.mount('#app')
