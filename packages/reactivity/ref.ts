b9b7973d0645a1e7fdf5205220509d144b4b79b5
tetracalibers <tomix05may@gmail.com>
2024-11-26 14:43:38 +0900

    isRef, refの型付けを強化（valueがなるべくanyにならないように）

--- packages/reactivity/ref.ts
+++ packages/reactivity/ref.ts
@@ -25,10 +25,13 @@ export interface Ref<T = any> {
   [RefSymbol]: true
 }
 
+export function isRef<T>(r: Ref<T> | unknown): r is Ref<T>
 export function isRef(r: any): r is Ref {
   return !!(r && r.__v_isRef === true)
 }
 
+export function ref<T = any>(): Ref<T | undefined>
+export function ref<T = any>(value: T): Ref<T>
 export function ref(value?: unknown) {
   return createRef(value)
 }
