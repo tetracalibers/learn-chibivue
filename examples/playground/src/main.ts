15dddc95e67fa193348164f8cc5b4d76f79e8b9b
tetracalibers <tomix05may@gmail.com>
2024-11-27 12:50:37 +0900

    [playground] それぞれ依存しているcomputedだけが発火することを確認

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,20 +1,26 @@
-import { createApp, h, reactive, toRefs } from 'chibivue'
+import { computed, createApp, h, reactive, ref } from 'chibivue'
 
 const app = createApp({
   setup() {
-    const state = reactive({ foo: 1, bar: 2 })
-    const stateAsRefs = toRefs(state)
+    const count = reactive({ value: 0 })
+    const count2 = reactive({ value: 0 })
+
+    const double = computed(() => count.value * 2)
+    const doubleDouble = computed(() => double.value * 2)
+
+    const countRef = ref(0)
+    const doubleCountRef = computed(() => countRef.value * 2)
 
     return () =>
       h('div', {}, [
-        h('p', {}, [`[state]: foo: ${state.foo}, bar: ${state.bar}`]),
-        h('p', {}, [
-          `[stateAsRefs]: foo: ${stateAsRefs.foo.value}, bar: ${stateAsRefs.bar.value}`,
-        ]),
-        h('button', { onClick: () => state.foo++ }, ['update state.foo']),
-        h('button', { onClick: () => stateAsRefs.bar.value++ }, [
-          'update stateAsRefs.bar.value',
-        ]),
+        h('pre', {}, [`count           : ${count.value}`]),
+        h('pre', {}, [`count2          : ${count2.value}`]),
+        h('pre', {}, [`count * 2       : ${double.value}`]),
+        h('pre', {}, [`(count * 2) * 2 : ${doubleDouble.value}`]),
+        h('pre', {}, [`countRef * 2    : ${doubleCountRef.value}`]),
+        h('button', { onClick: () => count.value++ }, ['update count']),
+        h('button', { onClick: () => count2.value++ }, ['update count2']),
+        h('button', { onClick: () => countRef.value++ }, ['update countRef']),
       ])
   },
 })
