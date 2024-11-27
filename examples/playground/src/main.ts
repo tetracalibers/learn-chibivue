681f32d0d642f9eeeb1f862e2b22d2f6b7d1f086
tetracalibers <tomix05may@gmail.com>
2024-11-27 13:33:51 +0900

    [playground] 書き込み可のcomputedの挙動を確認

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -1,26 +1,25 @@
-import { computed, createApp, h, reactive, ref } from 'chibivue'
+import { computed, createApp, h, ref } from 'chibivue'
 
 const app = createApp({
   setup() {
-    const count = reactive({ value: 0 })
-    const count2 = reactive({ value: 0 })
+    const count = ref(0)
 
-    const double = computed(() => count.value * 2)
-    const doubleDouble = computed(() => double.value * 2)
-
-    const countRef = ref(0)
-    const doubleCountRef = computed(() => countRef.value * 2)
+    const writeableDouble = computed<number>({
+      get: () => count.value * 2,
+      set: (val) => {
+        console.log('set double:', val)
+        count.value = val
+      },
+    })
 
     return () =>
       h('div', {}, [
-        h('pre', {}, [`count           : ${count.value}`]),
-        h('pre', {}, [`count2          : ${count2.value}`]),
-        h('pre', {}, [`count * 2       : ${double.value}`]),
-        h('pre', {}, [`(count * 2) * 2 : ${doubleDouble.value}`]),
-        h('pre', {}, [`countRef * 2    : ${doubleCountRef.value}`]),
+        h('pre', {}, [`count : ${count.value}`]),
+        h('pre', {}, [`double: ${writeableDouble.value}`]),
         h('button', { onClick: () => count.value++ }, ['update count']),
-        h('button', { onClick: () => count2.value++ }, ['update count2']),
-        h('button', { onClick: () => countRef.value++ }, ['update countRef']),
+        h('button', { onClick: () => writeableDouble.value++ }, [
+          'update double',
+        ]),
       ])
   },
 })
